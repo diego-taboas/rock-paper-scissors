30a5503578b09df459243832ca2d5efdb7fc498f
diego-taboas <diegolitaboas@gmail.com>
2025-03-27 20:12:49 -0300

    Fixed the computer choice function to work as intented

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,19 @@
 function getComputerChoice () {
-  return Math.floor(Math.random()* 3);
+  const computerChoice = Math.floor(Math.random()* 3);
+  if (computerChoice === 0) {
+    return "Rock";
+  } else if (computerChoice === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
+console.log(getComputerChoice())
 
 function getHumanChoice () {
-  let choice = prompt("Choose Rock, Paper or Scissors!");
+  let choice = prompt("Choose Rock, Paper or Scissors!").toUpperCase();
   return choice;
 }
+
+let humanScore = 0;
+let compuerScore = 0;
