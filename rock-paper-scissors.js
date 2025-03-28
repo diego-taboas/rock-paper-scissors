e8b633af69b8726c2639a6d71d5300e18b9c03ac
diego-taboas <diegolitaboas@gmail.com>
2025-03-28 01:32:35 -0300

    Forgot to commit often sorryyyy

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,19 +1,62 @@
-function getComputerChoice () {
-  const computerChoice = Math.floor(Math.random()* 3);
-  if (computerChoice === 0) {
+function getComputerChoice() {
+  const rdChoice = Math.floor(Math.random()* 3);
+  if (rdChoice === 0) {
     return "Rock";
-  } else if (computerChoice === 1) {
+  } else if (rdChoice === 1) {
     return "Paper";
   } else {
     return "Scissors";
   }
 }
-console.log(getComputerChoice())
 
-function getHumanChoice () {
-  let choice = prompt("Choose Rock, Paper or Scissors!").toUpperCase();
-  return choice;
+function getHumanChoice() {
+  const choice = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
+  return choice[0].toUpperCase() + choice.slice(1);
 }
 
-let humanScore = 0;
-let compuerScore = 0;
+function displayScores(humanScore, computerScore) {
+  console.log(`
+    Player Score = ${humanScore}
+    Computer Score = ${computerScore}
+    `)
+}
+
+function playGame () {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("You tied!");
+      displayScores(humanScore, computerScore);
+    } else if  (
+      (humanChoice === "Rock" && computerChoice === "Scissors") ||
+      (humanChoice === "Scissors" && computerChoice === "Paper") ||
+      (humanChoice === "Paper" && computerChoice === "Rock")
+    ) {
+      console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+      displayScores(humanScore, computerScore);
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+      displayScores(humanScore, computerScore);
+    }
+  }
+
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+
+  if (humanScore === computerScore) {
+    console.log(`You tied the Computer! Maybe next time you win!`);
+  } else if (humanScore > computerScore) {
+    console.log(`You won! Congratulations!`);
+  } else {
+    console.log(`You lost! But don't give up, try again!`);
+  }
+}
+
+playGame();
